10906fb0dd1cf3a7296ff39018cdd32144ae6e36
romanzhukovv <zhukov.roman89@gmail.com>
2022-02-23 12:33:20 +0300

    Initial Commit

---
+++ RGBViewSwiftUI/RGBViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBViewSwiftUIApp.swift
+//  RGBViewSwiftUI
+//
+//  Created by Roman Zhukov on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
